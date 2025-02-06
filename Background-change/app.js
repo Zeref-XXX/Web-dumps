356e666502daf87370134e388124ff3b82482d90
abhi.jeet <zereftemp@gmail.com>
2025-02-07 00:00:40 +0530

    back

---
+++ Background-change/app.js
@@ -0,0 +1,14 @@
+const e=document.getElementById("root");
+
+
+e.addEventListener( 'click',(event)=>{
+
+    e.innerText="hello";
+
+})
+
+// function print(){
+//     const e=document.getElementById("root");
+//     e.innerHTML="<h1>HELLO</h1>"
+    
+// }
