9ddda35ac0c4d3ca9cb8c4e85cdce26e239beedd
abhi.jeet <zereftemp@gmail.com>
2025-02-07 16:30:17 +0530

    added

--- Background-change/app.js
+++ Background-change/app.js
@@ -5,9 +5,10 @@ const h = document.querySelector('h1');
 
 reset.addEventListener('click', (event) => {
     document.body.style.backgroundColor = "whitesmoke";
-    // console.log(event.target);
+  
     h.style.backgroundColor="yellow";
     h.style.color = "white";
+    // console.log("white");
 
 })
 
